3493a41160f7ee3391292d495dc4750f8b923216
Hector Valles <hmvalles75@yahoo.com>
2019-07-15 10:54:08 -0700

    real questions

--- js/database.js
+++ js/database.js
@@ -1,31 +1,41 @@
 const db = {
   questions: [
     new Question(
-      "What language?",
-      ["Java", "Javascript", "C++", "C#"],
-      "Javascript"
+      "Who was the only non-member of the Beatles to receive a performance credit on a record??",
+      ["Eric Clapton", "Billy Preston", "Ravi Shanker", "Mick Jagger"],
+      "Billy Preston"
     ),
     new Question(
-      "sdhbwKQBDU",
+      "Who was the oldest Beatle?",
       [
-        "Prototypical Inheritance",
-        "For in loops",
-        "Teranaries",
-        "This Keyword"
+        "John",
+        "Paul",
+        "George",
+        "Ringo"
       ],
-      "Prototypical Inheritance"
+      "Ringo"
     ),
-    new Question("wit da most?", ["yes", "no", "maybe", "so"], "yes"),
+    new Question("Which of the following songs was not banned by the BBC?", 
+    ["I Am The Walrus",
+     "With A Little Help From My Friends",
+      "A Day In The Life",
+      "Lucky In The Sky With Diamonds"],
+       "A Day In The Life",
+    ),
     new Question(
-      "What is the best part of Javascript?",
+      "Which was the first Beatles song to feature backwards vocals?",
       [
-        "Prototypical Inheritance",
-        "For in loops",
-        "Teranaries",
-        "This Keyword"
+        "I'm Only Sleeping",
+        "Rain", 
+        "Tomorrow Never Knows",
+        "Strawberry Fields Forever"
       ],
-      "Prototypical Inheritance"
+      "Rain"
     ),
-    new Question("wit da most? ", ["yes", "no", "maybe", "so"], "yes")
+    new Question("Which of these is a Beatles middle name? ",
+    ["John",
+    "Paul",
+    "George",
+    "Ringo"], "George")
   ]
 };
